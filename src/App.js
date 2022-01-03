fafaefe5b7c2bf4e5a98f5cc232df6db3f78e637
Shawnathan McCourt <shawnmccourtb@gmail.com>
2022-01-02 23:49:00 -0500

    install dependencies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components/macro";
+import { GlobalStyle } from "./styles/global-styles";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Container = styled.div`
+  height: 100px;
+  width: 200px;
+  background-color: blue;
+`;
+
+const App = () => (
+  <>
+    <GlobalStyle />
+    <Container />
+  </>
+);
 
 export default App;
