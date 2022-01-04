1db0b8322681bb3dbc5683ada25fc2682df2d55d
Shawnathan McCourt <shawnmccourtb@gmail.com>
2022-01-04 11:44:49 -0500

    footer layout for mobile complete

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,26 +5,31 @@ import InstagramIcon from "../assets/shared/desktop/icon-instagram.svg";
 import TwitterIcon from "../assets/shared/desktop/icon-twitter.svg";
 import Navigation from "./Navigation";
 
+const FooterContainer = styled.footer`
+  background: linear-gradient(#d87d4a, #d87d4a) top / 25% 4px no-repeat, #000000;
+  position: fixed;
+  bottom: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 48px;
+  padding: 0 24px 38px;
+`;
+
+const DescriptionText = styled.p`
+  font-weight: 500;
+  line-height: 25px;
+  text-align: center;
+  color: #ffffff;
+  mix-blend-mode: normal;
+  opacity: 0.5;
+`;
+
 const Footer = () => {
-  const FooterContainer = styled.footer`
-    background-color: #000000;
-    position: fixed;
-    bottom: 0;
-  `;
-
-  const DescriptionText = styled.p`
-    font-weight: 500;
-    line-height: 25px;
-    text-align: center;
-    color: #ffffff;
-    mix-blend-mode: normal;
-    opacity: 0.5;
-  `;
-
   return (
     <FooterContainer>
-      <img src={Logo} alt="logo" />
-      <Navigation />
+      <img style={{ paddingTop: "52px" }} src={Logo} alt="logo" />
+      <Navigation footer />
       <DescriptionText>
         Audiophile is an all in one stop to fulfill your audio needs. We're a
         small team of music lovers and sound specialists who are devoted to
@@ -34,7 +39,7 @@ const Footer = () => {
       <DescriptionText as={"small"}>
         Copyright 2021. All Rights Reserved
       </DescriptionText>
-      <div>
+      <div style={{ display: "flex", gap: "16px" }}>
         <img src={FacebookIcon} alt={"facebook"} />
         <img src={TwitterIcon} alt={"twitter"} />
         <img src={InstagramIcon} alt={"instagram"} />
