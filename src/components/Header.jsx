546b595f0c287fd099135ad33c9f803f02819dc9
Shawnathan McCourt <shawnmccourtb@gmail.com>
2022-01-03 23:42:46 -0500

    complete mobile styles to Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,35 @@
 import Hamburger from "../assets/shared/tablet/icon-hamburger.svg";
 import Logo from "../assets/shared/desktop/logo.svg";
 import ShoppingCart from "../assets/shared/desktop/icon-cart.svg";
+import styled from "styled-components/macro";
+
+const HeaderContainer = styled.header`
+  height: 90px;
+  background-color: #000000;
+  position: fixed;
+  top: 0;
+  width: 100vw;
+  padding: 32px 24px 33px;
+  display: flex;
+  justify-content: center;
+`;
+
+const ImgContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+`;
 
 const Header = () => {
   return (
-    <header>
-      <img src={Hamburger} alt="menu" />
-      <img src={Logo} alt="logo" />
-      <img src={ShoppingCart} alt="shopping-cart" />
-    </header>
+    <HeaderContainer>
+      <ImgContainer>
+        <img style={{ height: "60%" }} src={Hamburger} alt="menu" />
+        <img src={Logo} alt="logo" />
+        <img src={ShoppingCart} alt="shopping-cart" />
+      </ImgContainer>
+    </HeaderContainer>
   );
 };
 
