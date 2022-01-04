9b0d325bf5f05c318d9c8da6af724e26d1725157
Shawnathan McCourt <shawnmccourtb@gmail.com>
2022-01-04 10:05:49 -0500

    add nav bar for desktop screen size

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import Hamburger from "../assets/shared/tablet/icon-hamburger.svg";
 import Logo from "../assets/shared/desktop/logo.svg";
 import ShoppingCart from "../assets/shared/desktop/icon-cart.svg";
 import styled from "styled-components/macro";
+import { useMediaQuery } from "react-responsive";
 
 const HeaderContainer = styled.header`
   height: 90px;
@@ -16,6 +17,9 @@ const HeaderContainer = styled.header`
   @media (min-width: 768px) {
     padding: 32px 40px 33px;
   }
+  @media (min-width: 1024px) {
+    padding: 32px 165px 37px;
+  }
 `;
 
 const ImgContainer = styled.div`
@@ -27,6 +31,10 @@ const ImgContainer = styled.div`
   @media (min-width: 768px) {
     justify-content: flex-start;
   }
+
+  @media (min-width: 1024px) {
+    justify-content: space-between;
+  }
 `;
 
 const HeaderImg = styled.img`
@@ -34,16 +42,43 @@ const HeaderImg = styled.img`
     margin-right: ${({ menu }) => menu && "42px"};
     margin-left: ${({ cart }) => cart && "auto"};
   }
+  @media (min-width: 1024px) {
+    margin-left: 0;
+  }
+`;
+
+const NavBar = styled.nav`
+  display: flex;
+  gap: 34px;
 `;
 
 const Header = () => {
+  const isLaptop = useMediaQuery({ query: "(min-width: 1024px)" });
   return (
     <HeaderContainer>
-      <ImgContainer>
-        <HeaderImg menu style={{ height: "60%" }} src={Hamburger} alt="menu" />
-        <img src={Logo} alt="logo" />
-        <HeaderImg cart src={ShoppingCart} alt="shopping-cart" />
-      </ImgContainer>
+      {isLaptop ? (
+        <ImgContainer>
+          <img src={Logo} alt="logo" />
+          <NavBar>
+            <a href={"#"}>Home</a>
+            <a href={"#"}>Headphones</a>
+            <a href={"#"}>Speakers</a>
+            <a href={"#"}>Earphones</a>
+          </NavBar>
+          <HeaderImg cart={"true"} src={ShoppingCart} alt="shopping-cart" />
+        </ImgContainer>
+      ) : (
+        <ImgContainer>
+          <HeaderImg
+            menu={"true"}
+            style={{ height: "60%" }}
+            src={Hamburger}
+            alt="menu"
+          />
+          <img src={Logo} alt="logo" />
+          <HeaderImg cart={"true"} src={ShoppingCart} alt="shopping-cart" />
+        </ImgContainer>
+      )}
     </HeaderContainer>
   );
 };
