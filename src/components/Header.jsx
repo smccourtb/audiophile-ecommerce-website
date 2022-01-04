01de4a85bd64d94330a0f80fb03f780d78e4cbb5
Shawnathan McCourt <shawnmccourtb@gmail.com>
2022-01-03 23:19:47 -0500

    ACW-1 Create header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,15 @@
+import Hamburger from "../assets/shared/tablet/icon-hamburger.svg";
+import Logo from "../assets/shared/desktop/logo.svg";
+import ShoppingCart from "../assets/shared/desktop/icon-cart.svg";
+
 const Header = () => {
-  return <header />;
+  return (
+    <header>
+      <img src={Hamburger} alt="menu" />
+      <img src={Logo} alt="logo" />
+      <img src={ShoppingCart} alt="shopping-cart" />
+    </header>
+  );
 };
 
 export default Header;
