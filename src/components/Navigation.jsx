4c7ed2d5267b18366c0367d2e4ce54de12899230
Shawnathan McCourt <shawnmccourtb@gmail.com>
2022-01-04 11:00:31 -0500

    create Navigation component and import to Header.jsx

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,13 @@
+import { Link, NavBar } from "../styles/shared-styles";
+
+const Navigation = () => {
+  return (
+    <NavBar>
+      <Link href={"#"}>Home</Link>
+      <Link href={"#"}>Headphones</Link>
+      <Link href={"#"}>Speakers</Link>
+      <Link href={"#"}>Earphones</Link>
+    </NavBar>
+  );
+};
+export default Navigation;
