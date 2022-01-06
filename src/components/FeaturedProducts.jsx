c2929a2184b5b0e8b314ec85fa7d8f487445bc2b
Shawnathan McCourt <shawnmccourtb@gmail.com>
2022-01-05 20:36:22 -0500

    complete featured section for desktop layout

--- src/components/FeaturedProducts.jsx
+++ src/components/FeaturedProducts.jsx
@@ -2,12 +2,17 @@ import styled from "styled-components/macro";
 import BgCircles from "../assets/home/desktop/pattern-circles.svg";
 import ZX9SpeakerImgMobile from "../assets/home/mobile/image-speaker-zx9.png";
 import ZX9SpeakerImgTablet from "../assets/home/tablet/image-speaker-zx9.png";
+import ZX9SpeakerImgDesktop from "../assets/home/desktop/image-speaker-zx9.png";
+
 import { Button } from "../styles/shared-styles";
 import ZX7SpeakerImgMobile from "../assets/home/mobile/image-speaker-zx7.jpg";
 import ZX7SpeakerImgTablet from "../assets/home/tablet/image-speaker-zx7.jpg";
+import ZX7SpeakerImgDesktop from "../assets/home/desktop/image-speaker-zx7.jpg";
 
 import YX1EarphonesImage from "../assets/home/mobile/image-earphones-yx1.jpg";
-import YX1EarphonesImgTablet from "../assets/home/tablet/image-earphones-yx1.jpg";
+import YX1EarphonesTablet from "../assets/home/tablet/image-earphones-yx1.jpg";
+
+import YX1EarphonesImgDesktop from "../assets/home/desktop/image-earphones-yx1.jpg";
 
 import { useMediaQuery } from "react-responsive";
 
@@ -38,6 +43,15 @@ const FeatureProductContainer = styled.div`
     background-position-x: center;
     background-size: 137%;
   }
+  @media (min-width: 1024px) {
+    flex-direction: row;
+    padding: 0;
+    height: 560px;
+    overflow: hidden;
+    background-position-y: -15px;
+    background-position-x: -175px;
+    background-size: 85%;
+  }
 `;
 
 const ProductTitle = styled.h3`
@@ -55,6 +69,10 @@ const ProductTitle = styled.h3`
     letter-spacing: 2px;
     padding-top: 40px;
   }
+  @media (min-width: 1024px) {
+    padding: 0 88px 24px 0;
+    text-align: left;
+  }
 `;
 
 const ProductDescription = styled.p`
@@ -67,6 +85,10 @@ const ProductDescription = styled.p`
   opacity: 0.75;
   @media (min-width: 768px) {
     padding-bottom: 16px;
+  }
+  @media (min-width: 1024px) {
+    padding-bottom: 40px;
+    text-align: left;
   }
 `;
 
@@ -82,6 +104,10 @@ const SecondaryProductContainer = styled.div`
   @media (min-width: 768px) {
     background: #f1f1f1 url(${ZX7SpeakerImgTablet});
     padding: 101px 423px 101px 62px;
+  }
+  @media (min-width: 1024px) {
+    background: #f1f1f1 url(${ZX7SpeakerImgDesktop});
+    padding-left: 92px;
   }
 `;
 
@@ -99,6 +125,9 @@ const ThirdProductContainer = styled.div`
     align-items: flex-start;
     justify-content: center;
     padding-left: 41px;
+  }
+  @media (min-width: 1024px) {
+    padding-left: 95px;
   }
 `;
 
@@ -119,6 +148,12 @@ const FeaturedImage = styled.img`
   @media (min-width: 768px) {
     width: 50%;
   }
+  @media (min-width: 1024px) {
+    width: 43%;
+    margin-bottom: -10px;
+    align-self: flex-end;
+    padding-left: 117.5px;
+  }
 `;
 
 const ProductImage = styled.img`
@@ -131,27 +166,58 @@ const ProductImage = styled.img`
 
 const FeaturedProducts = () => {
   const isTablet = useMediaQuery({ query: "(min-width: 768px)" });
+  const isDesktop = useMediaQuery({ query: "(min-width: 1024px)" });
+
   return (
     <FeaturedContainer>
       <FeatureProductContainer>
         <FeaturedImage
-          src={isTablet ? ZX9SpeakerImgTablet : ZX9SpeakerImgMobile}
+          src={
+            isDesktop
+              ? ZX9SpeakerImgDesktop
+              : isTablet
+              ? ZX9SpeakerImgTablet
+              : ZX9SpeakerImgMobile
+          }
           alt=""
         />
-        <ProductTitle>ZX9 SPEAKER</ProductTitle>
-        <ProductDescription>
-          Upgrade to premium speakers that are phenomenally built to deliver
-          truly remarkable sound.
-        </ProductDescription>
-        <Button>See Product</Button>
+        {isDesktop ? (
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              padding: "133px 95px 124px 138px",
+              alignItems: "flex-start",
+              textAlign: "left",
+            }}
+          >
+            <ProductTitle>ZX9 SPEAKER</ProductTitle>
+            <ProductDescription>
+              Upgrade to premium speakers that are phenomenally built to deliver
+              truly remarkable sound.
+            </ProductDescription>
+
+            <Button>See Product</Button>
+          </div>
+        ) : (
+          <>
+            <ProductTitle>ZX9 SPEAKER</ProductTitle>
+            <ProductDescription>
+              Upgrade to premium speakers that are phenomenally built to deliver
+              truly remarkable sound.
+            </ProductDescription>
+
+            <Button>See Product</Button>
+          </>
+        )}
       </FeatureProductContainer>
       <SecondaryProductContainer>
         <SecondaryTitle>XY7 Speaker</SecondaryTitle>
         <Button>See Product</Button>
       </SecondaryProductContainer>
-      {isTablet ? (
-        <div style={{ display: "flex", gap: "11px" }}>
-          <ProductImage src={YX1EarphonesImgTablet} />
+      {isDesktop ? (
+        <div style={{ display: "flex", gap: "30px" }}>
+          <ProductImage src={YX1EarphonesImgDesktop} />
           <ThirdProductContainer>
             <SecondaryTitle>YX1 Earphones</SecondaryTitle>
             <Button s>See Product</Button>
@@ -159,11 +225,23 @@ const FeaturedProducts = () => {
         </div>
       ) : (
         <>
-          <ProductImage src={YX1EarphonesImage} />
-          <ThirdProductContainer>
-            <SecondaryTitle>YX1 Earphones</SecondaryTitle>
-            <Button>See Product</Button>
-          </ThirdProductContainer>
+          {isTablet ? (
+            <div style={{ display: "flex", gap: "11px" }}>
+              <ProductImage src={YX1EarphonesTablet} />
+              <ThirdProductContainer>
+                <SecondaryTitle>YX1 Earphones</SecondaryTitle>
+                <Button s>See Product</Button>
+              </ThirdProductContainer>
+            </div>
+          ) : (
+            <>
+              <ProductImage src={YX1EarphonesImage} />
+              <ThirdProductContainer>
+                <SecondaryTitle>YX1 Earphones</SecondaryTitle>
+                <Button>See Product</Button>
+              </ThirdProductContainer>
+            </>
+          )}
         </>
       )}
     </FeaturedContainer>
