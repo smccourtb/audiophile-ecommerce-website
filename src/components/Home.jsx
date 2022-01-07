218a31f865a489dbb70a09e09332b687296536a9
Shawnathan McCourt <shawnmccourtb@gmail.com>
2022-01-06 23:13:12 -0500

    AUD-15 add the image links for the different categories

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,51 +1,16 @@
 import Subheader from "./Subheader";
-import CategoryLink from "./CategoryLink";
-import HeadphoneImage from "../assets/shared/desktop/image-category-thumbnail-headphones.png";
-import SpeakerImage from "../assets/shared/desktop/image-category-thumbnail-speakers.png";
-import EarphoneImage from "../assets/shared/desktop/image-category-thumbnail-earphones.png";
-import styled from "styled-components/macro";
+import CategoryLinks from "./CategoryLinks";
+
 import FeaturedProducts from "./FeaturedProducts";
 import Story from "./Story";
 import { Content } from "../styles/shared-styles";
 
-const CategorieContainer = styled.div`
-  display: flex;
-  gap: 16px;
-  flex-direction: column;
-  padding: 40px 0 80px 0;
-
-  @media (min-width: 768px) {
-    flex-direction: row;
-    gap: 10px;
-    padding: 96px 0 48px 0;
-  }
-
-  @media (min-width: 1024px) {
-    gap: 30px;
-  }
-`;
 const Home = () => {
   return (
     <>
       <Subheader />
       <Content>
-        <CategorieContainer>
-          <CategoryLink
-            image={HeadphoneImage}
-            title={"Headphones"}
-            target={"/headphones"}
-          />
-          <CategoryLink
-            image={SpeakerImage}
-            title={"Speakers"}
-            target={"/speakers"}
-          />
-          <CategoryLink
-            image={EarphoneImage}
-            title={"Earphones"}
-            target={"/earphones"}
-          />
-        </CategorieContainer>
+        <CategoryLinks />
         <FeaturedProducts />
         <Story />
       </Content>
