72025345dfddcc3ad69ba57fa728fb646d357eee
Shawnathan McCourt <shawnmccourtb@gmail.com>
2022-01-06 22:59:43 -0500

    AUD-14 create component for product display

--- src/components/HeadphonesPage.jsx
+++ src/components/HeadphonesPage.jsx
@@ -1,11 +1,35 @@
 import { Content } from "../styles/shared-styles";
 import SubheaderSimple from "./SubheaderSimple";
+import Product from "./Product";
+import XZ99IIMobile from "../assets/product-xx99-mark-two-headphones/mobile/image-category-page-preview.jpg";
+import XZ99IITablet from "../assets/product-xx99-mark-two-headphones/tablet/image-category-page-preview.jpg";
+import XZ99IIDesktop from "../assets/product-xx99-mark-two-headphones/desktop/image-category-page-preview.jpg";
+import styled from "styled-components/macro";
 
+const ContentContainer = styled(Content)`
+  gap: 120px;
+  padding: 64px 24px 120px 24px;
+`;
 const HeadphonesPage = () => {
   return (
     <>
       <SubheaderSimple text={"Headphones"} />
-      <Content>Headphone Page</Content>
+      <ContentContainer>
+        <Product
+          img={XZ99IIMobile}
+          name={"XX99 Mark II Headphones"}
+          description={`The new XX99 Mark II headphones is the pinnacle of pristine audio. It
+          redefines your premium headphone experience by reproducing the
+          balanced depth and precision of studio-quality sound.`}
+        />
+        <Product
+          img={XZ99IIMobile}
+          name={"XX99 Mark II Headphones"}
+          description={`The new XX99 Mark II headphones is the pinnacle of pristine audio. It
+          redefines your premium headphone experience by reproducing the
+          balanced depth and precision of studio-quality sound.`}
+        />
+      </ContentContainer>
     </>
   );
 };
---
+++ src/components/Product.jsx
@@ -0,0 +1,62 @@
+import { Button } from "../styles/shared-styles";
+import styled from "styled-components/macro";
+
+const ProductContainer = styled.article`
+  display: flex;
+  flex-direction: column;
+  gap: 32px;
+`;
+
+const TextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  gap: 24px;
+`;
+
+const NewProduct = styled.h2`
+  font-weight: normal;
+  font-size: 14px;
+  line-height: 19px;
+  text-align: center;
+  letter-spacing: 10px;
+  text-transform: uppercase;
+  color: #d87d4a;
+`;
+
+const ProductName = styled.h2`
+  font-weight: bold;
+  font-size: 28px;
+  line-height: 38px;
+  text-align: center;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  color: #000000;
+`;
+
+const ProductDescription = styled.p`
+  font-weight: 500;
+  font-size: 15px;
+  line-height: 25px;
+  text-align: center;
+  color: #000000;
+  mix-blend-mode: normal;
+  opacity: 0.5;
+`;
+
+const Product = ({ img, name, description }) => {
+  return (
+    <ProductContainer>
+      <img src={img} style={{ width: "100%" }} alt="" />
+      <TextContainer>
+        <NewProduct>New Product</NewProduct>
+        <ProductName>{name}</ProductName>
+        <ProductDescription>{description}</ProductDescription>
+        <Button primary>See Product</Button>
+      </TextContainer>
+    </ProductContainer>
+  );
+};
+
+export default Product;
