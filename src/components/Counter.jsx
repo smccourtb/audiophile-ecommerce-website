b9090730a182fd270b60cdc096a9d4618a9731f5
Shawnathan McCourt <shawnmccourtb@gmail.com>
2022-01-11 17:15:30 -0500

    add full height to buttons on Counter.jsx

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -12,21 +12,19 @@ const CounterContainer = styled.div`
 
 const Button = styled.button`
   width: 33%;
+  height: 100%;
   border: none;
   font-style: normal;
   font-weight: bold;
   font-size: 13px;
   line-height: 18px;
-  /* identical to box height */
-
   text-align: center;
   letter-spacing: 1px;
   text-transform: uppercase;
-
   color: #000000;
-
   mix-blend-mode: normal;
   opacity: 0.25;
+  cursor: pointer;
 `;
 
 const Number = styled.p`
