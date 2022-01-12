29a14d2f3a856542a8a9b67216fee6d1c11da06f
Shawnathan McCourt <shawnmccourtb@gmail.com>
2022-01-12 10:09:14 -0500

    refactor data logic

--- src/App.js
+++ src/App.js
@@ -4,61 +4,50 @@ import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 // main pages of content
-import Home from "./components/Home";
-import HeadphonesPage from "./components/HeadphonesPage";
-import SpeakersPage from "./components/SpeakersPage";
-import EarphonesPage from "./components/EarphonesPage";
-import Checkout from "./components/Checkout";
+import Home from "./pages/Home";
+import CategoryPage from "./pages/CategoryPage";
+import Checkout from "./pages/Checkout";
 import { ContentContainer } from "./styles/shared-styles";
 import data from "./data.json";
-import ProductDetailPage from "./components/ProductDetailPage";
+import ProductDetail from "./pages/ProductDetail";
+import GlobalState from "./context/GlobalState";
 
-const headphoneData = [data[3], data[2], data[1]];
-const speakerData = [data[4], data[5]];
-const earphoneData = data[0];
+const headphoneData = data
+  .filter((product) => product.category === "headphones")
+  .sort((a, b) => b.id - a.id);
+const speakerData = data.filter((product) => product.category === "speakers");
+const earphoneData = data.filter((product) => product.category === "earphones");
+const categories = [headphoneData, speakerData, earphoneData];
+const App = () => {
+  const productPages = data.map((product) => (
+    <Route
+      path={`/${product.slug}`}
+      element={<ProductDetail data={product} />}
+    />
+  ));
 
-const App = () => (
-  <>
-    <GlobalStyle />
-    <Header />
-    <ContentContainer>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route
-          path="/headphones"
-          element={<HeadphonesPage data={headphoneData} />}
-        />
-        <Route path="/speakers" element={<SpeakersPage />} />
-        <Route path="/earphones" element={<EarphonesPage />} />
-        <Route path="/checkout" element={<Checkout />} />
-        <Route
-          path="/xx59-headphones"
-          element={<ProductDetailPage data={headphoneData[2]} />}
-        />
-        <Route
-          path="/xx99-mark-two-headphones"
-          element={<ProductDetailPage data={headphoneData[0]} />}
-        />
-        <Route
-          path="/xx99-mark-one-headphones"
-          element={<ProductDetailPage data={headphoneData[1]} />}
-        />
-        <Route
-          path="/zx7-speaker"
-          element={<ProductDetailPage data={speakerData[0]} />}
-        />
-        <Route
-          path="/zx9-speaker"
-          element={<ProductDetailPage data={speakerData[1]} />}
-        />
-        <Route
-          path="/yx1-earphones"
-          element={<ProductDetailPage data={earphoneData} />}
-        />
-      </Routes>
-    </ContentContainer>
-    <Footer />
-  </>
-);
+  const categoryPages = categories.map((category) => (
+    <Route
+      path={category[0].category}
+      element={<CategoryPage data={category} />}
+    />
+  ));
+
+  return (
+    <GlobalState>
+      <GlobalStyle />
+      <Header />
+      <ContentContainer>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/checkout" element={<Checkout />} />
+          {categoryPages}
+          {productPages}
+        </Routes>
+      </ContentContainer>
+      <Footer />
+    </GlobalState>
+  );
+};
 
 export default App;
