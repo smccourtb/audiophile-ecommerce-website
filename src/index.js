17989b07855965d1593f47ec1afb0cbf8909002d
Shawnathan McCourt <shawnmccourtb@gmail.com>
2022-01-13 15:00:07 -0500

    move global state wrapper to index.js and import NiceModal package

--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import NiceModal from "@ebay/nice-modal-react";
+import GlobalState from "./context/GlobalState";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <GlobalState>
+        <NiceModal.Provider>
+          <App />
+        </NiceModal.Provider>
+      </GlobalState>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
