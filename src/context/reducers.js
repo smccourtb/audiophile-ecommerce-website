e694c655dca834d3b2c0925421cdf018bd9dc42d
Shawnathan McCourt <shawnmccourtb@gmail.com>
2022-01-13 15:03:13 -0500

    create functions to update item quantity in hte cart and to clear the cart

--- src/context/reducers.js
+++ src/context/reducers.js
@@ -1,13 +1,13 @@
 export const ADD_PRODUCT = "ADD_PRODUCT";
 export const REMOVE_PRODUCT = "REMOVE_PRODUCT";
+export const UPDATE_QUANTITY = "UPDATE_QUANTITY";
+export const EMPTY_CART = "EMPTY_CART";
 
 const addProductToCart = (product, amount, state) => {
-  console.log("adding product", product.name);
   const updatedCart = [...state.cart];
   const updatedItemIndex = updatedCart.findIndex(
     (item) => item.id === product.id
   );
-
   if (updatedItemIndex < 0) {
     updatedCart.push({ ...product, quantity: amount });
   } else {
@@ -17,12 +17,10 @@ const addProductToCart = (product, amount, state) => {
     updatedItem.quantity += amount;
     updatedCart[updatedItemIndex] = updatedItem;
   }
-  console.log("CART: ", { ...updatedCart });
   return { ...state, cart: updatedCart };
 };
 
-const removeProductFromCart = (productId, state) => {
-  console.log("remove product: " + productId);
+const removeProductFromCart = (productId, amount, state) => {
   const updatedCart = [...state.cart];
   const updatedItemIndex = updatedCart.findIndex(
     (item) => item.id === productId
@@ -31,7 +29,8 @@ const removeProductFromCart = (productId, state) => {
   const updatedItem = {
     ...updatedCart[updatedItemIndex],
   };
-  updatedItem.quantity--;
+  const amountToRemove = updatedItem.quantity - amount + 1;
+  updatedItem.quantity -= amountToRemove;
   if (updatedItem.quantity <= 0) {
     updatedCart.splice(updatedItemIndex, 1);
   } else {
@@ -41,15 +40,34 @@ const removeProductFromCart = (productId, state) => {
   return { ...state, cart: updatedCart };
 };
 
+export const updateQuantity = (productId, amount, state) => {
+  const updatedCart = [...state.cart];
+  const updatedItemIndex = updatedCart.findIndex(
+    (item) => item.id === productId
+  );
+  const updatedItem = {
+    ...updatedCart[updatedItemIndex],
+  };
+  const amountToAdd = amount - updatedItem.quantity + 1;
+  updatedItem.quantity += amountToAdd;
+  updatedCart[updatedItemIndex] = updatedItem;
+  return { ...state, cart: updatedCart };
+};
+
+export const clearCart = (state) => {
+  return { ...state, cart: [] };
+};
+
 export const shopReducer = (state, action) => {
   switch (action.type) {
     case ADD_PRODUCT:
-      console.log("ACTION: ", action);
       return addProductToCart(action.product, action.amount, state);
-
     case REMOVE_PRODUCT:
-      return removeProductFromCart(action.productId, state);
-
+      return removeProductFromCart(action.productId, action.amount, state);
+    case UPDATE_QUANTITY:
+      return updateQuantity(action.productId, action.amount, state);
+    case EMPTY_CART:
+      return clearCart(state);
     default:
       return state;
   }
